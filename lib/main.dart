9407b41f123850a06e80b2e74abe1c6cd35445e8
rubayet211 <rubayet211@gmail.com>
2023-08-21 15:16:02 +0600

    2nd Commit

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ImageList());
+}
+
+class ImageList extends StatefulWidget {
+  @override
+  ImageListState createState() => ImageListState();
+}
+
+class ImageListState extends State<ImageList> {
+  List<String> _imagePath = [
+    'assets/images/apple.png',
+    'assets/images/pineapple.png',
+    'assets/images/Cherries.png',
+    'assets/images/pear.png',
+    'assets/images/Kiwi.png',
+  ];
+
+  List<String> _images = [];
+
+  void _addImage() {
+    setState(
+      () {
+        if (_images.length < _imagePath.length) {
+          _images.add(_imagePath[_images.length]);
+        }
+      },
+    );
+  }
+
+  void _removeImage(int i) {
+    setState(() {
+      _images.removeAt(i);
+    });
+  }
+
+  void _deleteImage() {
+    setState(() {
+      _images.removeLast();
+    });
+  }
+
+  void _clearImage() {
+    setState(() {
+      _images.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Text(
+            "Image Show",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _deleteImage,
+          child: Icon(Icons.delete),
+          backgroundColor: Colors.teal,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 25.0),
+          child: Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: _addImage,
+                  child: Text('Add Image'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.teal),
+                    fixedSize: MaterialStateProperty.all<Size>(
+                      Size(150, 50),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _images.length,
+                    itemBuilder: (context, index) {
+                      return Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                _images[index],
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            child: IconButton(
+                              icon: Icon(Icons.close),
+                              onPressed: () => _removeImage(index),
+                            ),
+                            right: 0.0,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.restart_alt_rounded),
+                  iconSize: 30,
+                  onPressed: _clearImage,
+                  style: ButtonStyle(
+                    iconSize: MaterialStateProperty.all<double>(150),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
